88846e2913d7f1d297b08ff7aa1014f7dbb60d3d
Priyank Bhuva <160010116006@adit.ac.in>
2019-01-18 00:20:38 +0530

    localhost

--- app/utils/config.js
+++ app/utils/config.js
@@ -8,7 +8,7 @@ module.exports = {
     mongoDB : {
         user: 'admin',
         password: 'shreeji1',
-        host : '34.73.92.20',
+        host : 'localhost',
         port : 27017,
         database : 'spectrum'
     },   
