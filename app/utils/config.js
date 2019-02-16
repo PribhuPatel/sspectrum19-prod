5aadb06a8e80274ac6b7fc6a3bd62c71d967a9e4
Priyank Bhuva <160010116006@adit.ac.in>
2019-02-16 17:13:17 +0530

    qrapp set and qrcode final

--- app/utils/config.js
+++ app/utils/config.js
@@ -9,10 +9,10 @@ module.exports = {
         user: 'admin',
         password: 'shreeji1',
         // host : '34.73.92.20',
-        host:'ds135003.mlab.com',
-        port:35003,
-        // host:'localhost',
-        // port : 27017,
+        // host:'ds135003.mlab.com',
+        // port:35003,
+        host:'localhost',
+        port : 27017,
         database : 'spectrum'
     },   
      nodemailer:{
