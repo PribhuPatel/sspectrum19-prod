2fdd77687da8c98e60c5d3eec5a1250566332c1c
Priyank Bhuva <160010116006@adit.ac.in>
2019-02-16 17:47:11 +0530

    qrcode final

--- app/qrcode.js
+++ app/qrcode.js
@@ -63,10 +63,10 @@ var config2= {mongoDB : {
     user: 'admin',
     password: 'shreeji1',
     // host : '34.73.92.20',
-    host:'ds135003.mlab.com',
-    port:35003,
-    // host:'localhost',
-    // port : 27017,
+    // host:'ds135003.mlab.com',
+    // port:35003,
+    host:'localhost',
+    port : 27017,
     database : 'spectrum'
 }
 }
@@ -121,7 +121,7 @@ var nt2 =async()=>{
     }
     console.log(i);
     i++;
-    if(i<3){
+    if(i<1000){
         setTimeout(nt2,10000);
     }
 }
